4e8a2824f670f6eb0bfb3ee57684d6ff7286636f
satoharuki0744 <hotsuakari@gmail.com>
2020-03-04 22:14:07 +0900

    照明関係の実装

--- MyRoomIoT/MyRoomIoT/MainPageViewModel.cs
+++ MyRoomIoT/MyRoomIoT/MainPageViewModel.cs
@@ -11,6 +11,9 @@ namespace MyRoomIoT
     {
         private string _labelText;
 
+        public ICommand SendLightOn { get; }
+        public ICommand SendLightOff { get; }
+        public ICommand SendLightNight { get; }
         public ICommand SendStartHot { get; }
         public ICommand SendStopAir { get; }
         public string LabelText
@@ -24,8 +27,11 @@ namespace MyRoomIoT
         }
         public MainPageViewModel()
         {
-            SendStopAir = new Send(actionMaker("stop_air.dat"));
-            SendStartHot = new Send(actionMaker("start_hot.dat"));
+            SendStopAir = new Send(actionMaker(MyData.STOP_AIR));
+            SendStartHot = new Send(actionMaker(MyData.START_HOT));
+            SendLightOn = new Send(actionMaker(MyData.LIGHT_ON));
+            SendLightOff = new Send(actionMaker(MyData.LIGHT_OFF));
+            SendLightNight = new Send(actionMaker(MyData.LIGHT_Night));
             LabelText = "ここにHTMLレスポンスが表示されます";
         }
 
