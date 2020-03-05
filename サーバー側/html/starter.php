2e325610eb18b16c80d5b471fff47d7521fc7b7e
satoharuki0744 <hotsuakari@gmail.com>
2020-03-05 17:35:28 +0900

    説明を加筆

--- サーバー側/html/starter.php
+++ サーバー側/html/starter.php
@@ -1,7 +1,7 @@
 <?php
      $pass =$_POST["pass"];
      $data =escapeshellarg($_POST["data"]); 
-     $pass_hash = "fb0be50d6e9a1e65d5717baade2fef09ab2a2f40ab4e6e315f403cabd1f20245";
+     $pass_hash = "";#パスワードのハッシュ値を入力しておいてください
      $command ="sudo sh data/run.sh ";
      if (hash('sha256',$pass)== $pass_hash){
          exec($command.$data,$output);
